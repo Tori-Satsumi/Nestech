0ae0bff0620150a995b234cf864f5653a24c0694
aimless cat <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-21 08:16:33 +0000

    Feb 21, 2024, 3:16 PM

--- week4/bootcamp/day1/ex4_4.py
+++ week4/bootcamp/day1/ex4_4.py
@@ -49,16 +49,16 @@ def solve():
 
     # 88 sec
     # amount of value: 442232
-    for a in range(1, 10):
-        for b in range(1, 10):
-            for c in range(1, 10):
-                for d in range(1, 10):
-                    for e in range(1, 10):
-                        for f in range(1, 10):
-                            for g in range(1, 10):
-                                for h in range(1, 10):
-                                    for i in range(1, 10):
-                                        if (a + 13 * b / c) + (d + 12 * e - f)+ (g * h / i) == 87:
+    for b in range(1,10):
+        for c in range(1,10):
+            for d in range(1,10):
+                for e in range(1,10):
+                    for f in range(1,10):
+                        for g in range(1,10):
+                            for h in range(1,10):
+                                for i in range(1,10):
+                                    a = - 13 * b / c - d - 12 * e + f + 11 - g * h / i + 10 - 66
+                                    if a > 0 and a < 10:
                                             cnt += 1
 
     print(f"time: {time.time() - start} sec")

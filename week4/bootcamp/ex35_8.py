ee073411b974952e9922f0449d7db4babc6f71cb
aimless cat <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-21 07:00:50 +0000

    Feb 21, 2024, 2:00 PM

--- week4/bootcamp/ex35_8.py
+++ week4/bootcamp/ex35_8.py
@@ -33,10 +33,10 @@ def solve(N):
     for i, row in enumerate(lst):
         for ind, char in enumerate(lst[i]):
             if not(ind == i or ind == (N - 1 - i)):
-                lst[i][ind] = '*'
+                lst[i][ind].replace(char, "*")
 
-    # for i in lst:
-    #     print(i)
+    for i in lst:
+        print(i)
 
     return None
 

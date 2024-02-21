e50728e32ce262e4d08b6a6f9cdfc5c23d20e83b
aimless cat <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-21 03:13:14 +0000

    Feb 21, 2024, 10:13 AM

--- week4/bootcamp/ex5_9.py
+++ week4/bootcamp/ex5_9.py
@@ -77,7 +77,7 @@ def solve(input_data):
     sắp xếp theo thứ tự giảm dần.
     """
 
-    ls1 = [(city["name"], city["population"]) for city in sorted(data.items(), key=lambda item: item[1]) if city["name"][0].lower() == "h"]
+    ls1 = [(city["name"], city["population"]) for city in data if city["name"][0].lower() == "h"]
 
     for i in ls1:
         print(i)

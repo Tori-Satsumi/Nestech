670d918a0fbe246e771c2cd76c6b67057927d2c8
aimless cat <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-21 06:44:17 +0000

    Feb 21, 2024, 1:44 PM

--- week4/bootcamp/ex35_7.py
+++ week4/bootcamp/ex35_7.py
@@ -11,14 +11,11 @@ def solve(N):
     Must: use list comprehension
     Tips: list comprehension always create new list
     """
-    lst = [[i for i in range(_i)] for _i in range(N)]
-    return [ls for ls in lst if not sum(ls) % 3]
-
-    return len([[i] for i in range(N) if not (sum([i]) % 3) or not (sum([i]) % 5)])
+    return [ls for ls in [[i for i in range(1, _i)] for _i in range(1, N)] if (not (sum(ls) % 3) or not (sum(ls) % 5)) and ls]
 
 
 def main():
-    print(solve(10))
+    print(solve(10000))
 
 
 if __name__ == "__main__":

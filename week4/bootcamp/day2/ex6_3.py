55425019d50a08ceec665d0f7ec6d772633692f6
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-25 14:48:06 +0000

    Feb 25, 2024, 2:48 PM

--- week4/bootcamp/day2/ex6_3.py
+++ week4/bootcamp/day2/ex6_3.py
@@ -8,7 +8,7 @@ def find_max_price(datafile):
     with open(datafile, "r") as f:
         hld = {"time" : None, "price": "0", "UNKNOWN" : None}
         for row in csv.DictReader(f, ["time", "price", "UNKNOWN"]): # NOQA
-            print(int(row["price"]))
+            print(int(float(row["price"])))
 
     print(hld)
 

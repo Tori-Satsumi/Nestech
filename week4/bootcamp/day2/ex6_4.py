87a419f83cf2a2175173733b24878a1f4d677b3f
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-26 04:58:03 +0000

    Feb 26, 2024, 4:58 AM

--- week4/bootcamp/day2/ex6_4.py
+++ week4/bootcamp/day2/ex6_4.py
@@ -6,19 +6,29 @@ def sumall(*input_data):
     hoặc string) được gọi. Thay input_data bằng code phù hợp.
     """
     
-    return sum([round(float(i), 2) for i in input_data if str(i).isnumeric()])
+    return sum([round(float(i), 2) for i in input_data if check_num(i)])
 
-def convert_flt(num):
+
+def check_num(num):
     try:
-        num = int(float(num)) 
+        _ = int(float(num)) 
         return True
     except:
         return False
 
 
 def solve():
+    lst = [
+        10,
+        10.2,
+        10.,
+        .0,
+        ".7",
+        "abc",
+        r".1",
+    ]
 
-    return None
+    return sumall(*lst)
 
 
 def main():

9cdf754ddb7809dec59a191953074517fb223cb9
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-26 06:46:55 +0000

    Feb 26, 2024, 6:46 AM

--- week4/bootcamp/day2/ex6_5.py
+++ week4/bootcamp/day2/ex6_5.py
@@ -15,7 +15,9 @@ def your_function(datapath):
     """
     with urllib.request.urlopen(" https://api.github.com/repos/saltstack/salt/contributors?page=4") as url:
         data = json.load(url)
-        print(data)
+        with open("info.json", "w") as jsfile:
+            json.dump(data, jsfile)
+            # print(data)
 
 
     return None

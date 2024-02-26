ab171c2c2beb3d9a96a44bdbf98d76f30cc700f6
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-26 15:15:00 +0000

    Feb 26, 2024, 3:15 PM

--- week4/bootcamp/day2/ex6_9.py
+++ week4/bootcamp/day2/ex6_9.py
@@ -69,8 +69,8 @@ def your_function(path) -> Dict[str, int]:
                 result[ext] = 0
 
             try:
-                ls = import_module(name)
-                print(name)
+                ls = import_module(nm)
+                print(ls)
                 # if isfunction(func):
                 #     print(func)
             except:

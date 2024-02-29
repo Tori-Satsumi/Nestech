9af37a9514eeed773ea38bbea6639fdceeda1621
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-29 05:08:03 +0000

    Feb 29, 2024, 5:08 AM

--- week4/bootcamp/day2/tst.py
+++ week4/bootcamp/day2/tst.py
@@ -1,12 +1,12 @@
 import configparser
 config = configparser.ConfigParser()
 
-# config.read('config_file.ini')
-# sample = config.get('text_file_required','file1')
-# with open(sample, "r") as f:
-#     print(f.read())
+config.read('config_file.ini')
+sample = config.get('text_file_required','file1')
+with open(sample, "r") as f:
+    print(f.read())
 
-data = open('ex8_2.txt', 'r+')
-a = configparser.ConfigParser()
-a.read_file(data)
-print(a)
+# data = open('event.yaml', 'r+')
+# a = configparser.ConfigParser()
+# a.read_file(data)
+# print(a)

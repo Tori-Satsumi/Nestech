92b7a7fa1a9066d2a3b6053779f4938d35d18f5b
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-02-29 09:09:16 +0000

    Feb 29, 2024, 9:09 AM

--- week4/bootcamp/day2/ex8_8.py
+++ week4/bootcamp/day2/ex8_8.py
@@ -38,12 +38,12 @@ def get_version(input_data: str) -> str:
     :rtype str:
     """
     # Sửa tên và function cho phù hợp, trả về kết quả yêu cầu.
-    print(input_data)
-    if validate(input_data):
-        m, d, y = input_data.split("/")
-        print(m, d, y)
-    else:
-        raise ValueError("Invalid date")
+    print(datetime.strptime(input_data, '%m/%d/%Y'))
+    # if validate(input_data):
+    #     m, d, y = input_data.split("/")
+    #     print(m, d, y)
+    # else:
+    #     raise ValueError("Invalid date")
 
 
 def solve(input_data):

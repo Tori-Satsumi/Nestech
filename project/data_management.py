3ebb5a0ed5fa43fc6011b48042c1019ad4b66f5f
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-04-25 09:23:06 +0000

    Apr 25, 2024, 9:23 AM

--- project/data_management.py
+++ project/data_management.py
@@ -1,11 +1,21 @@
 import mysql.connector
+from mysql.connector import errorcode
 
-mydb = mysql.connector.connect(
-    host="127.0.0.1",
-    port=3306,
-    user="FREE-SPIRIT",
-    passwd="1234",
-    db="test"
-)
+try:
+    mydb = mysql.connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="FREE-SPIRIT",
+        passwd="1234",
+        db="test"
+    )
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+    print(mydb)
 
-print(mydb)

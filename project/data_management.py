84f3a317ef16bdbe1286b732f197da04a4dd6fe5
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-04-25 09:21:01 +0000

    Apr 25, 2024, 9:21 AM

--- project/data_management.py
+++ project/data_management.py
@@ -3,8 +3,8 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host="127.0.0.1",
     port=3306,
-    user="yoruname",
-    passwd="yourpwd",
+    user="FREE-SPIRIT",
+    passwd="1234",
     db="test"
 )
 

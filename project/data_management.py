b387f65ac3be1b27c1143e612503cdf5478fa0a8
Tori-Satsumi <132991636+Tori-Satsumi@users.noreply.github.com>
2024-04-25 09:19:21 +0000

    Apr 25, 2024, 9:19 AM

--- project/data_management.py
+++ project/data_management.py
@@ -1,9 +1,10 @@
 import mysql.connector
 
 mydb = mysql.connector.connect(
-  host="localhost",
-  user="yourusername",
-  password="yourpassword"
+    host="127.0.0.1",
+    user="db_username",
+    passwd="db_password",
+    db="db_name"
 )
 
 print(mydb)
